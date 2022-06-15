c5cf5529b2e3a24aea78215f23c1f571f63859f9
FahmidaNourin <75219480+FahmidaNourin@users.noreply.github.com>
2022-06-15 20:09:40 +0600

    Create Practice_1.sql

---
+++ Self_Practice/Class 2/Practice_1.sql
@@ -0,0 +1,43 @@
+CREATE TABLE Customer(
+    CustomerID INT(2),
+    CustomerName VARCHAR(15),
+    ContactName VARCHAR(10),
+    Address VARCHAR(20),
+    City VARCHAR(8),
+    PostalCode VARCHAR(9),
+    Country VARCHAR(10)
+
+);
+
+
+INSERT INTO `customer`(`CustomerID`, `CustomerName`, `ContactName`, `Address`, `City`, `PostalCode`, `Country`) VALUES (1,'Nourin','Fahmida','Gazipur','Berlin','1220','Germany')
+
+
+INSERT INTO `customer`(`CustomerID`, `CustomerName`, `ContactName`, `Address`, `City`, `PostalCode`, `Country`) VALUES (2,'Noumi','Fahmid','Gazipura','Barlin','1320','Italy')
+
+INSERT INTO `customer`(`CustomerID`, `CustomerName`, `ContactName`, `Address`, `City`, `PostalCode`, `Country`) VALUES (3,'Nou','Fahmi','Gonzipur','Betterlin','1120','Spain')
+
+INSERT INTO `customer`(`CustomerID`, `CustomerName`, `ContactName`, `Address`, `City`, `PostalCode`, `Country`) VALUES (4,'Noufi','mida','Gazi','Balin','1420','Bali')
+
+INSERT INTO `customer`(`CustomerID`, `CustomerName`, `ContactName`, `Address`, `City`, `PostalCode`, `Country`) VALUES (5,'Noura','Fima','Garlipur','Relin','1520','India')
+
+INSERT INTO `customer`(`CustomerID`, `CustomerName`, `ContactName`, `Address`, `City`, `PostalCode`, `Country`) VALUES (6,'Nourin1','Fahmida1','Gazipur','Berlin','1220','Germany')
+
+
+INSERT INTO `customer`(`CustomerID`, `CustomerName`, `ContactName`, `Address`, `City`, `PostalCode`, `Country`) VALUES (7,'Noumi2','Fahmid2','Gazipura','Barlin','1320','Italy')
+
+INSERT INTO `customer`(`CustomerID`, `CustomerName`, `ContactName`, `Address`, `City`, `PostalCode`, `Country`) VALUES (8,'Nou3','Fahmi3','Gonzipur','Betterlin','1120','Spain')
+
+INSERT INTO `customer`(`CustomerID`, `CustomerName`, `ContactName`, `Address`, `City`, `PostalCode`, `Country`) VALUES (9,'Noufi4','mida4','Gazi','Balin','1420','Bali')
+
+INSERT INTO `customer`(`CustomerID`, `CustomerName`, `ContactName`, `Address`, `City`, `PostalCode`, `Country`) VALUES (10,'Noura5','Fima5','Garlipur','Relin','1520','India')
+
+
+
+
+
+
+
+
+
+
