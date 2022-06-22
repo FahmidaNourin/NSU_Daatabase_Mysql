64bdae2b715e803fab63618d9ef1d7f084834f16
FahmidaNourin <75219480+FahmidaNourin@users.noreply.github.com>
2022-06-22 20:17:08 +0600

    Update practice.sql

--- Self_Practice/class_3/practice.sql
+++ Self_Practice/class_3/practice.sql
@@ -5,9 +5,14 @@ CREATE TABLE EMPLOYEE(
     ID INT(4) NOT NULL,
     Name VARCHAR(20) NOT NULL ,
     Department VARCHAR(5) NOT NULL,
-    Salary DECIMAL(5,2) NOT NULL,
+    Salary DECIMAL(6,2) NOT NULL,
     Gender VARCHAR(5) NOT NULL,
     Age INT(2) NOT NULL,
     City VARCHAR(8)
 
 );
+INSERT INTO  EMPLOYEE(ID, Name, Department,Salary,Gender,Age,City) VALUES
+                     (1001,'JON DOE','IT',4000.30,'Male',25,'London'),
+                      (1011,'Hi Hello','EE',5000.50,'Male',27,'Jorpukor');
+ 
+ 
