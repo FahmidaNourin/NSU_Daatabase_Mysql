885a18f8ab219f2a9a94a2492fef44e4aa906ad5
FahmidaNourin <75219480+FahmidaNourin@users.noreply.github.com>
2022-06-22 19:57:45 +0600

    Update practice.sql

--- Self_Practice/class_3/practice.sql
+++ Self_Practice/class_3/practice.sql
@@ -2,5 +2,12 @@ TOPIC:Arithmetic Expressions,Defining a Column Alias,Eliminating Duplicate Rows,
 Displaying Table Structure,Concatenation Operator
 
 CREATE TABLE EMPLOYEE(
-    ID 
+    ID NUMBER(4) NOT NULL,
+    Name VARCHAR(20) NOT NULL ,
+    Department VARCHAR(5) NOT NULL,
+    Salary NUMBER(5,2) NOT NULL,
+    Gender VARCHAR(5) NOT NULL,
+    Age NUMBER(2) NOT NULL,
+    City VARCHAR(8)
+
 );
