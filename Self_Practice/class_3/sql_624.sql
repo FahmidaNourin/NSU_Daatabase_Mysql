b4a4575a6d76c3a1221d68d11ffaaa77bca7ad5d
FahmidaNourin <75219480+FahmidaNourin@users.noreply.github.com>
2022-06-24 11:39:32 +0600

    Create sql_624.sql

---
+++ Self_Practice/class_3/sql_624.sql
@@ -0,0 +1,12 @@
+CREATE TABLE Person (
+    Person_ID INT(1) Not Null,
+    LastName VARCHAR(7)Not Null,
+    FirstName VARCHAR(6)Not Null,
+    Age INT(2)Not Null,
+    Adress VARCHAR(6)Not Null,
+    Email VARCHAR(6)Not Null,
+    Contact_Number INT(15)Not Null,
+    
+    Primary key(Person_ID)
+    
+);
