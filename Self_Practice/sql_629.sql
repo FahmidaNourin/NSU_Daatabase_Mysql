56751c47034b685bf3de2902e3e0a2d7c06165c9
FahmidaNourin <75219480+FahmidaNourin@users.noreply.github.com>
2022-06-29 09:36:38 +0600

    Update sql_629.sql

--- Self_Practice/sql_629.sql
+++ Self_Practice/sql_629.sql
@@ -5,4 +5,41 @@ CREATE TABLE departments
     ) ;
 
 
-    
+
+CREATE TABLE employees
+   ( employee_id INTEGER
+   , first_name VARCHAR(20)
+   , last_name VARCHAR(25) 
+   , email VARCHAR(25)
+   , phone_number VARCHAR(20)
+   , hire_date DATE
+   , job_id VARCHAR(10)
+   , salary INTEGER
+   , commission_pct INTEGER
+   , manager_id INTEGER
+   , department_id INTEGER
+   , constraint pk_emp primary key (employee_id) 
+   , constraint fk_deptno foreign key (department_id) references departments(department_id)  
+   ) ;
+
+
+
+INSERT INTO departments VALUES ( 20,'Marketing',  180);
+ INSERT INTO departments VALUES ( 30,'Purchasing',  1700);
+ INSERT INTO departments VALUES ( 40, 'Human Resources',  2400);
+ INSERT INTO departments VALUES ( 50, 'Shipping',  1500);
+ INSERT INTO departments VALUES ( 60 , 'IT',  1400);
+ INSERT INTO departments VALUES ( 70, 'Public Relations',  2700);
+ INSERT INTO departments VALUES ( 80 , 'Sales',  2500 );
+ INSERT INTO departments VALUES ( 90 , 'Executive',  1700);
+ INSERT INTO departments VALUES ( 100 , 'Finance',  1700);
+ INSERT INTO departments VALUES ( 110 , 'Accounting',  1700);
+ INSERT INTO departments VALUES ( 120 , 'Treasury' ,  1700);
+ INSERT INTO departments VALUES ( 130 , 'Corporate Tax' ,  1700 );
+ INSERT INTO departments VALUES ( 140, 'Control And Credit' ,  1700);
+ INSERT INTO departments VALUES ( 150 , 'Shareholder Services', 1700);
+ INSERT INTO departments VALUES ( 160 , 'Benefits', 1700);
+ INSERT INTO departments VALUES ( 170 , 'Payroll' , 1700);
+
+
+ 
